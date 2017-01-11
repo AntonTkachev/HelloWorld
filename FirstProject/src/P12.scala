59b92ca8d50300e34ee640d3bb71c27795140c86
AntonTkachev <dgeneros@mail.ru>
2017-01-11 11:28:05 +0300

    Added 14 and 15 task from 99 Scala Problems

--- FirstProject/src/P12.scala
+++ FirstProject/src/P12.scala
@@ -11,9 +11,9 @@ object P12 extends App {
     * res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
     **/
 
-  def decode(l: List[(Int, Symbol)]) = {
+  def decode(list: List[(Int, Symbol)]) = {
     var endList = new ListBuffer[Symbol]()
-    l.foreach(x => {
+    list.foreach(x => {
       val res = List.fill(x._1)(x._2)
       res.foreach(s => endList += s)
     })
@@ -21,4 +21,38 @@ object P12 extends App {
   }
 
   decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+}
+
+object P14 extends App {
+
+  /**
+    * P14 (*) Duplicate the elements of a list.
+    * Example:
+    * scala> duplicate(List('a, 'b, 'c, 'c, 'd))
+    * res0: List[Symbol] = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+    **/
+
+  def duplicate(list: List[Symbol], numOfIter: Int = 2) = {
+    var endList = new ListBuffer[Symbol]()
+    list.foreach(x => {
+      for (i <- 1 to numOfIter) {
+        endList += x
+      }
+    })
+    println(endList)
+  }
+
+  duplicate(List('a, 'b, 'c, 'c, 'd))
+}
+
+object P15 extends App {
+
+  /**
+    * P15 (**) Duplicate the elements of a list a given number of times.
+    * Example:
+    * scala> duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+    * res0: List[Symbol] = List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
+    **/
+
+  P14.duplicate(List('a, 'b, 'c, 'c, 'd), 3)
 }
